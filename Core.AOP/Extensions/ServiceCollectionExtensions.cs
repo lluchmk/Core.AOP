be6b3c49ffb0d88c7a8274b70f798a6557d4284f
Kevin Lluch Martinez <lluchmk@gmail.com>
2018-12-02 20:49:38 +0100

    Initial commit. Basic functionality.

---
+++ Core.AOP/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using static Core.AOP.ProxyGeneration.ProxyResolver;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTransientWithInterceptors<TService, TImplementation>(this IServiceCollection services)
+           where TService : class
+           where TImplementation : class, TService
+            => services.AddTransient<TService>(_p => ResolveProxy<TImplementation>(_p));
+
+        public static IServiceCollection AddScopedWithInterceptors<TService, TImplementation>(this IServiceCollection services)
+           where TService : class
+           where TImplementation : class, TService
+            => services.AddScoped<TService>(_p => ResolveProxy<TImplementation>(_p));
+
+        public static IServiceCollection AddSingletonWithInterceptors<TService, TImplementation>(this IServiceCollection services)
+           where TService : class
+           where TImplementation : class, TService
+            => services.AddSingleton<TService>(_p => ResolveProxy<TImplementation>(_p));
+    }
+}
---
+++ Core.AOP/ProxyGeneration/ProxyResolver.cs
@@ -0,0 +1,67 @@
+﻿using Castle.DynamicProxy;
+using Core.AOP.Atributes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Core.AOP.ProxyGeneration
+{
+    internal static class ProxyResolver
+    {
+        internal static T ResolveProxy<T>(IServiceProvider serviceProvider)
+            where T : class
+        {
+            var interceptors = ResolveInterceptors<T>(serviceProvider);
+            return GenerateProxy<T>(serviceProvider, interceptors);
+        }
+
+        private static IInterceptor[] ResolveInterceptors<T>(IServiceProvider serviceProvider)
+        {
+            var interceptorTypes = typeof(T).GetCustomAttributes<InterceptableAttribute>().Select(attr => attr.InterceptorType);
+            return GenerateInterceptors(serviceProvider, interceptorTypes);
+        }
+
+        private static IInterceptor[] GenerateInterceptors(IServiceProvider serviceProvider, IEnumerable<Type> interceptorTypes)
+        {
+            var interceptors = new IInterceptor[interceptorTypes.Count()];
+            for (int i = 0; i < interceptors.Length; i++)
+            {
+                interceptors[i] = GenerateInterceptor(serviceProvider, interceptorTypes.ElementAt(i));
+            }
+            return interceptors;
+        }
+
+        private static IInterceptor GenerateInterceptor(IServiceProvider serviceProvider, Type interceptorType)
+        {
+            var interceptorCtor = interceptorType.GetConstructors()[0];
+            var interceptorParams = ResolveConstructorParameters(serviceProvider, interceptorCtor);
+            return (IInterceptor)interceptorCtor.Invoke(interceptorParams);
+        }
+
+        private static T GenerateProxy<T>(IServiceProvider serviceProvider, IInterceptor[] interceptors)
+        {
+            var proxyGenerator = new ProxyGenerator();
+            var ctrParams = ResolveTypeConstructorParameters(serviceProvider, typeof(T));
+            return (T)proxyGenerator.CreateClassProxy(typeof(T), ctrParams, interceptors.ToArray());
+        }
+
+        private static object[] ResolveConstructorParameters(IServiceProvider provider, ConstructorInfo ctor)
+        {
+            var ctrParams = ctor.GetParameters();
+            var resolvedParams = new object[ctrParams.Count()];
+            for (int i = 0; i < resolvedParams.Length; i++)
+            {
+                resolvedParams[i] = ActivatorUtilities.GetServiceOrCreateInstance(provider, ctrParams[i].ParameterType);
+            }
+            return resolvedParams;
+        }
+
+        private static object[] ResolveTypeConstructorParameters(IServiceProvider serviceProvider, Type type)
+        {
+            var ctor = type.GetConstructors()[0];
+            return ResolveConstructorParameters(serviceProvider, ctor);
+        }
+    }
+}
